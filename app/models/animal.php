471facbf410b37d84fd94f74f36c2ccc78749897
matiusgg <newpowerexperience2016@gmail.com>
2019-05-20 13:04:41 +0200

    Primer commit

---
+++ app/models/animal.php
@@ -0,0 +1,11 @@
+<?php
+
+// SUPERCLASE: BAse de donde se crearan mas 
+
+class animal {
+
+    // PROPIEDADES
+
+    public $id
+}
+?>
---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="css/index.css">
+    <title>Document</title>
+</head>
+<body>
+    <h1>
+    ESCOGE TU PERRO
+    </h1>
+
+    <section>
+    <article>
+    <div>
+    
+    <span>
+    <img src="img/perro.jpg" alt="">
+    </span>
+    
+    </div>
+
+    <div>
+    
+    </div>
+    
+    </article>
+    
+    </section>
+</body>
+</html>
